2669fb960856e7ea872e3e6deca7fe71cabd4a4f
leeool <lgonsalez02@gmail.com>
2022-03-14 00:13:23 -0300

    finaliza o exercicio

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string cebolinha;
+
+Console.Write("Digite uma frase: ");
+cebolinha = Console.ReadLine()!;
+
+cebolinha = cebolinha
+    .Replace("r", "l")
+    .Replace("R", "L");
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write($"\n{cebolinha}");
+Console.ResetColor();
+
